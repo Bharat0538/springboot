540db27d61f1ed05a32e219efb06381dcb89b98a
Bharat0538 <ahirwarbharat.0538@gmail.com>
2022-09-10 15:02:41 +0530

    Cloud gateway api added.

---
+++ cloud-gateway-api/src/test/java/com/mp/cg/api/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mp.cg.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
